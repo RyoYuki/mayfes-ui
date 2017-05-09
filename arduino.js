75cd541864449f548f833ee2593cce8d65ca2321
Takahashi Ryo <ryo.t.5974@gmail.com>
2017-05-09 05:36:24 +0100

    improve client and implement server

--- arduino.js
+++ arduino.js
@@ -1,30 +1,31 @@
-var SerialPort = require("serialport").SerialPort;
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+/*
+var SerialPort = require("serialport");
 var serialPort = new SerialPort("COM13", {
   baudrate: 9600
 });
 
 serialPort.on('open', function () {
+  if (serialport)
   console.log('serial open');
 });
+*/
 
-function startGame() {
-    serialPort.write("s\n", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });
-}
+app.post('/send', function(req, res) {
+  res.send('You sent the name "' + req.body.name + '".');
+  console.log("test");
+  /*
+  serialPort.write("s\n", function(err, results) {
+    console.log('err ' + err);
+    console.log('results ' + results);
+  });
+  */
+});
 
-function stopGame() {
-    serialPort.write("x\n", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });
-}
-
-function resetGame() {
-    serialPort.write("r\n", function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-    });
-}
-
+app.listen(8080, function() {
+  console.log('Server running at http://127.0.0.1:8080/');
+});
